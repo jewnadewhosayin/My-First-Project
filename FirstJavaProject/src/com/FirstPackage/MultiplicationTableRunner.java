8ba0dc1bf609deddf1e46eb1738e5776f1b888c9
Junaid <Junaid@localhost>
2020-12-14 20:29:55 -0600

    FirstJavaProject
    -FirstJavaProject

---
+++ FirstJavaProject/src/com/FirstPackage/MultiplicationTableRunner.java
@@ -0,0 +1,12 @@
+package com.FirstPackage;
+
+public class MultiplicationTableRunner {
+	
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		table.print();
+		table.print(3);
+		table.print(6,11,20);
+	}
+
+}
