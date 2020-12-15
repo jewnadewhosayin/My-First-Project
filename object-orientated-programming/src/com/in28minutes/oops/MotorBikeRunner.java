13625450d02ec53a8cdb9f390e6806b08e6a9c58
Junaid <Junaid@localhost>
2020-12-14 20:33:29 -0600

    OOPS
    -AlisOOPSpushtogit

---
+++ object-orientated-programming/src/com/in28minutes/oops/MotorBikeRunner.java
@@ -0,0 +1,12 @@
+package com.in28minutes.oops;
+
+public class MotorBikeRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MotorBike ducati = new MotorBike();
+		ducati.start();
+
+	}
+
+}
